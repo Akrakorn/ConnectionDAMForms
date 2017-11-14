4003b1fa7d6f4c8c854bc4aa2dd482bdbfdf7a12
Arnau Tarragó <arnautarragollorach@gmail.com>
2017-11-14 10:47:24 +0100

    Bug fixes. :)

--- ConnectionDAMForms/ConnectionDAMForms/ClErrors.cs
+++ ConnectionDAMForms/ConnectionDAMForms/ClErrors.cs
@@ -7,7 +7,7 @@ using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
 
-namespace ConnectionDam
+namespace ConnectionDAMForms
 {
     class ClErrors
     {
--- ConnectionDAMForms/ConnectionDAMForms/ClPort.cs
+++ ConnectionDAMForms/ConnectionDAMForms/ClPort.cs
@@ -4,7 +4,7 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace ConnectionDam
+namespace ConnectionDAMForms
 {
     public class ClPort
     {
